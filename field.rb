dfbff8d76ee850285f141ab0246256d1fab79870
jeffrothwell <rothwelljeff@gmail.com>
2017-12-28 15:09:40 -0500

    transfer class variables to simulator class, write harvest method

--- field.rb
+++ field.rb
@@ -1,17 +1,20 @@
 class Field
-  attr_accessor :type, :size
-
-  @@fields = []
-  @@total_harvest = 0
+  attr_accessor :type, :size, :field_harvest
 
   def initialize(type, size)
     @type = type
     @size = size
+    @field_harvest = 0
   end
 
-  def create(type, size)
-    new_field = Field.new(type, size)
-    @@fields << new_field
+  def harvest(field)
+    if field.type = "corn"
+      amount_harvested = field.size * 20
+    elsif field.type = "wheat"
+      amount_harvested = field.size * 30
+    end
+    field.field_harvest += amount_harvested
+    return amount_harvested
   end
 
 end
